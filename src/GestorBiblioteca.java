4f2e2fa4e8a10a3f7fd62b7d8cd02e264c50234e
Mykola Makovii <mykola.makovii@insbaixcamp.cat>
2026-05-10 21:41:49 +0200

    Implementada la gestió de préstecs

--- src/GestorBiblioteca.java
+++ src/GestorBiblioteca.java
@@ -2,19 +2,53 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 public class GestorBiblioteca {
+
+    // ---------------------------------
+    // ---------- ATRIBUTOS ------------
+    // ---------------------------------
+
     private List<Prestec> prestecs;
+
+    // ---------------------------------
+    // --------- CONSTRUCTOR -----------
+    // ---------------------------------
+
     public GestorBiblioteca() {
         this.prestecs = new ArrayList<>();
     }
+
+    // ---------------------------------
+    // ---------- FUNCIONES ------------
+    // ---------------------------------
+
     public void prestarLlibre(Usuari usuari, Llibre llibre) {
-        if (!llibre.esPrestat()) {
-            llibre.prestar();
-            Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
-            prestecs.add(prestec);
-            usuari.afegirLlibre(llibre);
-            System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
+        if (usuari.getLlibresPrestats().size() >= Prestec.MAX_LLIBRES) {
+            System.out.println("L'usuari ha arribat al límit de llibres.");
+            return;
         } else {
-            System.out.println("Aquest llibre ja està prestat.");
+            if (!llibre.disponible()) {
+                System.out.println("Aquest llibre no està disponible.");
+                return;
+            } else {
+                llibre.prestar();
+
+                Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
+
+                prestecs.add(prestec);
+
+                usuari.afegirLlibre(llibre);
+
+                usuari.afegirPrestecHistorial(prestec);
+                System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
+            }
         }
+    }
+
+    // ---------------------------------
+    // ------- GETTERS + SETTERS -------
+    // ---------------------------------
+
+    public List<Prestec> getPrestecs() {
+        return prestecs;
     }
 }
